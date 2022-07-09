d8f560ec0ae8410a03837d37664135b1bae7607a
FIXER3600 <guilhermecomercial51@gmail.com>
2022-07-09 17:37:32 -0300

    Arrumando porta e schema do banco para deploy

--- src/data/BaseDatabase.ts
+++ src/data/BaseDatabase.ts
@@ -12,10 +12,10 @@ export abstract class BaseDatabase {
                 client: "mysql",
                 connection: {
                   host: process.env.DB_HOST,
-                  port: 3306,
+                  port: process.env.PORT || 3306,
                   user: process.env.DB_USER,
                   password: process.env.DB_PASSWORD,
-                  database: process.env.DB_NAME,
+                  database: process.env.DB_SCHEMA,
                 },
               });        
         }
